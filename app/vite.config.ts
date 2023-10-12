2bb54da0a9eb328d685ba903042288b8dfd486b5
Chris <48444574+CMorenoStokoe@users.noreply.github.com>
2023-10-12 23:36:14 +0100

    Set website base

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  base: "https://morenostok.io/kat/dist/app/",
+
   cacheDir: './node_modules/.vite/app',
 
   server: {
