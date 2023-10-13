42bfddcc0dab4933d98cf9ba8ee0dac8cfc4c73e
Chris <48444574+CMorenoStokoe@users.noreply.github.com>
2023-10-13 12:04:41 +0100

    Set dynamic base

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-  base: "https://morenostok.io/kat/dist/app/",
+  base: "/kat/app/",
 
   cacheDir: './node_modules/.vite/app',
 
@@ -18,16 +18,4 @@ export default defineConfig({
   },
 
   plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  test: {
-    globals: true,
-    cache: { dir: './node_modules/.vitest' },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
 });
